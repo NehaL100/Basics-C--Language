7d2995c523688058e37d135ddd048474696389d2
Muhammad Nehal <99079068+NehaL100@users.noreply.github.com>
2022-02-10 15:06:21 +0500

    Add files via upload

---
+++ printf and scanf.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>  
+     int main()
+{
+	
+	int a,b;
+	printf("Enter any two  numbers\n");
+	scanf("%d" ,&a);
+	scanf("%d" ,&b);	
+	printf("addition of two numbers is= %d",a+b);
+	return 0;
+	
+}
